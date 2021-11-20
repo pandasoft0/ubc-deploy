b4412bbba755c655fdcc56a385d015a781ef4e64
= <=>
2021-11-19 21:32:11 -0500

    gh-pages

---
+++ precache-manifest.05bb32a84953b29b8397d163d711556b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "badbdb18b82034a1fea6fd718cc5d47c",
+    "url": "/ubc-deploy/UC-logo2.png"
+  },
+  {
+    "revision": "0eb34bdc1a75fa9303c4",
+    "url": "/ubc-deploy/css/app.css"
+  },
+  {
+    "revision": "aa80a480d408739d3a49",
+    "url": "/ubc-deploy/css/chunk-vendors.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ubc-deploy/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ubc-deploy/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ubc-deploy/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ubc-deploy/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/ubc-deploy/img/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "41c270db43e9a70a64b2c16378df62ae",
+    "url": "/ubc-deploy/img/hamburger.png"
+  },
+  {
+    "revision": "badbdb18b82034a1fea6fd718cc5d47c",
+    "url": "/ubc-deploy/img/logo.png"
+  },
+  {
+    "revision": "9caab8470689e57cbb5a71e07bbd2d99",
+    "url": "/ubc-deploy/img/rising.png"
+  },
+  {
+    "revision": "831695af364febfb725d56ed5eef8e39",
+    "url": "/ubc-deploy/index.html"
+  },
+  {
+    "revision": "0eb34bdc1a75fa9303c4",
+    "url": "/ubc-deploy/js/app.js"
+  },
+  {
+    "revision": "b27a2ffbea2fd0f1403f",
+    "url": "/ubc-deploy/js/chunk-1c26ff1a.js"
+  },
+  {
+    "revision": "70c99b5a45f0223fb1cd",
+    "url": "/ubc-deploy/js/chunk-48a30608.js"
+  },
+  {
+    "revision": "d1d55f524412c09b355a",
+    "url": "/ubc-deploy/js/chunk-62d55034.js"
+  },
+  {
+    "revision": "aa80a480d408739d3a49",
+    "url": "/ubc-deploy/js/chunk-vendors.js"
+  },
+  {
+    "revision": "59b4fc26343074884653a2af117147f9",
+    "url": "/ubc-deploy/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/ubc-deploy/robots.txt"
+  }
+]);
